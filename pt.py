9aa371b3717ceeb9cfb6d096f1b6628d02af5a61
GauravTandi <148497273+GauravTandi@users.noreply.github.com>
2026-06-11 09:33:22 +0000

    added tkinter to notify timer finished

--- pt.py
+++ pt.py
@@ -1,5 +1,10 @@
-import sys
-import time
+import sys, time
+import tkinter as tk
+from tkinter import messagebox
+
+root = tk.Tk()
+root.withdraw() 
+
 
 timer_set = sys.argv[1]
 hr_min_sec = sys.argv[2]
@@ -7,20 +12,24 @@ hr_min_sec = sys.argv[2]
 if "hr" in hr_min_sec:
     print(f"setting time for {timer_set}hours")
     timer_set = int(timer_set * 60) * 60
+    time.sleep(timer_set)
     print(f"{timer_set} hours timer finished.")
+    messagebox.showinfo(title="Pomodoro Timer",message="Great work, timer finished")
 
 elif "min" in hr_min_sec:
     print(f"setting time for {timer_set} minutes")
     timer_set = int(timer_set) * 60 
     time.sleep(timer_set)
     print(f"{timer_set} minutes timer finished.")
+    messagebox.showinfo(title="Pomodoro Timer",message="Great work, timer finished")
 
 elif "sec" in hr_min_sec:
     print(f"setting time for {timer_set} seconds")
     time.sleep(int(timer_set))
     print(f"{timer_set} seconds timer finished.")
+    messagebox.showinfo(title="Pomodoro Timer",message="Great work, timer finished")
 
 else:
-    print("usage: \n")
+    print("usage:- \npython pt.py 20min \npython pt.py 2 her  \npython pt.py 10 sec")
 
-print(timer_set)
+root.destroy()
