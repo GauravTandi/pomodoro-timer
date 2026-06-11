cea364e842a8c642c2abc5ab03e0faa59cc0a4b7
GauravTandi <148497273+GauravTandi@users.noreply.github.com>
2026-06-11 08:26:33 +0000

    added timer and did little maths to calculate sleep time

--- pt.py
+++ pt.py
@@ -5,12 +5,21 @@ timer_set = sys.argv[1]
 hr_min_sec = sys.argv[2]
 
 if "hr" in hr_min_sec:
-    print("setting time for hours")
+    print(f"setting time for {timer_set}hours")
+    timer_set = int(timer_set * 60) * 60
+    print(f"{timer_set} hours timer finished.")
+
 elif "min" in hr_min_sec:
-    print("setting time for minutes")
-    time.sleep(5)
+    print(f"setting time for {timer_set} minutes")
+    timer_set = int(timer_set) * 60 
+    time.sleep(timer_set)
+    print(f"{timer_set} minutes timer finished.")
+
 elif "sec" in hr_min_sec:
-    print("setting time for seconds")
+    print(f"setting time for {timer_set} seconds")
+    time.sleep(int(timer_set))
+    print(f"{timer_set} seconds timer finished.")
+
 else:
     print("usage: \n")
 
